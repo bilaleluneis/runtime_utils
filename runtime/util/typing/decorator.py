430902d858e4b36ddd260f133623b58ce0f6f303
Jieshu <foundwonder@gmail.com>
2020-01-07 20:47:52 -0700

    minor changes

--- runtime/util/typing/decorator.py
+++ runtime/util/typing/decorator.py
@@ -1,10 +1,10 @@
-from typing import Callable, Any, TypeVar, cast, get_type_hints
+from typing import Callable, Any, TypeVar, cast, get_type_hints, Tuple, Type, List
 from functools import wraps
 from runtime.util.typing._runtime_type_checker import _check_types, _ParameterInfo
 
-__author__ = "Bilal El Uneis"
+__author__ = "Bilal El Uneis & Jieshu Wang"
 __since__ = "Jan 2020"
-__email__ = "bilaleluneis@gmail.com"
+__email__ = "bilaleluneis@gmail.com, foundwonder@gmail.com"
 
 FuncType = Callable[..., Any]
 F = TypeVar('F', bound=FuncType)
@@ -21,7 +21,7 @@ def runtimechecked(function: F) -> F:
         if len(params_value):
             type_hints = get_type_hints(function)
             type_hints.pop('return', None)  # remove the return type from hints for now
-            type_hints_list = list(type_hints.items())
+            type_hints_list = cast(List[Tuple[str, Type[Any]]], list(type_hints.items()))
             params_info = [_ParameterInfo(name=func_sig[0], expected_type=func_sig[1], actual_value=value)
                            for func_sig, value in zip(type_hints_list, params_value)]
             result = _check_types(params_info)
